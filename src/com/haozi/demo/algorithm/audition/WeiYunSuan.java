3471abe5153089e74b6872451ea76247ad3f2399
haoziapple <haozixiaowang@163.com>
2017-12-22 22:19:58 +0800

    update 位运算测试-位运算、补码的机制需要再学习

---
+++ src/com/haozi/demo/algorithm/audition/WeiYunSuan.java
@@ -0,0 +1,13 @@
+package com.haozi.demo.algorithm.audition;
+
+/**
+ * Created by ASUS on 2017/12/22.
+ */
+public class WeiYunSuan {
+    public static void main(String[] args) {
+        int a =2;
+        int b=3;
+        System.out.println(Integer.toString(a & -a));
+        System.out.println(Integer.toString(b & -b));
+    }
+}
